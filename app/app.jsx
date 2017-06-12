f283a19025a59f2ace43be29bc0fc151241ec291
Crystal Lu <syyssie@gmail.com>
2017-06-12 20:54:48 +0800

    Create navigation bar

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var About = require('About');
+var Main = require('main');
 var MemoApp = require('MemoApp');
 // Load foundation
 $(document).foundation();
@@ -10,6 +12,11 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-  <MemoApp/>,
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+      <Route path="about" component={About}/>
+      <IndexRoute component={MemoApp}/>
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
---
+++ app/components/Navigation.jsx
@@ -0,0 +1,43 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Memo App
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Memo</IndexLink>
+          </li>
+          <li>
+            <Link to="/about" activeClassName="active-link">About</Link>
+          </li>
+          <li>
+            <form>
+              <ul className="menu">
+                <li>
+                  <input type="search" placeholder="Search memo by name" ref="search"/>
+                </li>
+                <li>
+                  <input type="submit" className="button" value="Get Memo"/>
+                </li>
+              </ul>
+            </form>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">
+            Created by <a href="http://www.mead.io" target="_blank">Crystal Lu</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+module.exports = Navigation;
